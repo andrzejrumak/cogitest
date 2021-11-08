3f0ed0e740d8b10c633a24a65a2c30386590dd56
Andrzej Rumak <ajgltl@gmail.com>
2021-11-08 19:49:52 +0100

    getCurrentPage - pagination, Tailwind CSS

--- src/store/index.js
+++ src/store/index.js
@@ -7,19 +7,12 @@ const headers = { Accept: 'application/json' }
 const store = createStore({
   state: {
     allPosts: [],
-    currentPage: [],
-    currentPost: [],
     allUsers: []
-    // currentUser: [],
-    // currentUserName: ''
   },
   mutations: {
     setAllPosts (state, payload) {
       state.allPosts = payload
     },
-    // setCurrentPage (state, payload) {
-    //   state.currentPage = payload
-    // },
     setAllUsers (state, payload) {
       state.allUsers = payload
     }
@@ -30,41 +23,30 @@ const store = createStore({
       const posts = await res.json()
       state.commit('setAllPosts', posts)
     },
-    // setCurrentPage (state, id) {
-    //   // const posts = () => { return state.allPosts.find(post => post.id === id) }
-    //   const posts = (state) => (id) => state.allPosts.find(post => post.id === id)
-    //   state.commit('setCurrentPage', posts)
-    // },
+
     async setAllUsers (state) {
       const res = await fetch(urlUsers, { headers })
       const users = await res.json()
       state.commit('setAllUsers', users)
     },
-    // async setCurrentUser (state, id) {
-    //   const res = await fetch(urlUsers + '/' + id, { headers })
-    //   const user = await res.json()
-    //   state.commit('setCurrentUser', user)
-    // },
     errasePosts (state) {
       state.commit('setAllPosts', [])
     }
-
   },
   modules: {},
   getters: {
     getAllPosts: state => state.allPosts,
     getAllUsers: state => state.allUsers,
-    getCurrentPost: state => (state) => (id) => {
+    getCurrentPost: (state) => (id) => {
       return state.allPosts.find(post => post.id === id)
     },
     getTotalPosts: state => state.allPosts.length,
     getCurrentUserName: (state) => (id) => {
       return (state.allUsers.find(user => user.id === id)).name
+    },
+    getCurrentPage: (state) => (id) => {
+      return state.allPosts.slice(id * 10, id * 10 + 10)
     }
-    // getCurrentPage: (state) => state.currentPage
-    // getCurrentPage: (state) => (id) => {
-    //   return state.allPosts.find(post => post.id === id)
-    // }
   }
 })
 
